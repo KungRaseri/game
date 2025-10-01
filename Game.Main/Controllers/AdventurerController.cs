f69bcd81b2ded2992483a26a3e5346217e3c8ca7
KungRaseri <countjoshington@gmail.com>
2025-10-01 10:42:07 -0700

    Remove redundant goblin creation in AdventurerController for cleaner monster initialization

--- Game.Main/Controllers/AdventurerController.cs
+++ Game.Main/Controllers/AdventurerController.cs
@@ -52,7 +52,6 @@ public class AdventurerController : IDisposable
         {
             EntityFactory.CreateGoblin(),
             EntityFactory.CreateGoblin(),
-            EntityFactory.CreateGoblin(),
             EntityFactory.CreateGoblin()
         };
 
